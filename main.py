d5d0fe411e2ee3c39d698bd5c5dc6aedd1758479
Paul Theceri <paul.theceri@gmail.com>
2021-03-13 19:32:00 +0300

    small fixes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from configs.base_config import Development, Staging, Production
 from flask_sqlalchemy import SQLAlchemy
-# import psycopg2
 
 app = Flask(__name__)
 app.config.from_object(Development)
@@ -34,38 +33,11 @@ def itemlisting():
         db.session.add(a)
         db.session.commit()
         print("Record successfully added")
+        
         return redirect(url_for('itemlisting'))
         
     items = Item.query.all()
-    return render_template('items.html', items = items)    
-    
-        
-# class Item(db.Model):
-#     __tablename__ = 'items'
-#     id = db.Column(db.Integer, primary_key=True)
-#     item_name = db.Column(db.String(80), unique=False, nullable=False)
-#     item_quantity = db.Column(db.Integer, unique=False)
-#     item_buying_price = db.Column(db.Integer, unique=False)
-#     item_selling_price = db.Column(db.Integer, unique=False)
-
-# @app.route('/', methods = ['GET', 'POST'])
-# def itemlisting():
-#     if request.method == 'GET':
-#         return render_template('items.html')
-#     else:
-#         item_name = request.form['item_name']
-#         item_quantity = request.form['item_quantity']
-#         item_buying_price = request.form['item_buying_price']
-#         item_selling_price = request.form['item_selling_price']
-
-#         # print(item_name, item_quantity, item_buying_price, item_selling_price)
-
-#         a = Item(item_name = item_name, item_quantity = item_quantity, item_buying_price = item_buying_price, item_selling_price = item_selling_price)
-#         db.session.add(a)
-#         db.session.commit()
-#         print("Record successfully added")
-        
-#         return redirect(url_for('itemlisting'))
+    return render_template('items.html', items = items)
 
 if __name__ == '__main__':
     app.run(debug = True)
